62d03d570ab247a1a12486bbf799fb31442b8423
siddy-dev <siddybuddy18@gmail.com>
2020-03-13 12:54:28 +0530

    4 files modified, 1 added, 1 deleted

---
+++ src/components/courses/CoursesPage.js
@@ -0,0 +1,40 @@
+import React from "react";
+import { connect } from "react-redux";
+import * as courseActions from "../../redux/actions/courseActions";
+import PropTypes from "prop-types";
+import { bindActionCreators } from "redux";
+
+class CoursesPage extends React.Component {
+  render() {
+    return (
+      <>
+        <h2>Courses</h2>
+        {this.props.courses.map(course => (
+          <div key={course.title}>{course.title}</div>
+        ))}
+      </>
+    );
+  }
+}
+
+CoursesPage.propTypes = {
+  courses: PropTypes.array.isRequired,
+  actions: PropTypes.object.isRequired
+};
+
+function mapStateToProps(state) {
+  return {
+    courses: state.courses
+  };
+}
+
+function mapDispatchToProps(dispatch) {
+  return {
+    actions: bindActionCreators(courseActions, dispatch)
+  };
+}
+
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(CoursesPage);
--- src/redux/actions/courseActions.js
+++ src/redux/actions/courseActions.js
@@ -1,5 +1,21 @@
 import * as types from "./actionTypes";
+import * as courseApi from "../../api/courseApi";
 
 export function createCourse(course) {
   return { type: types.CREATE_COURSE, course };
+}
+
+export function loadCourseSuccess(courses) {
+  return { type: types.LOAD_COURSES_SUCCESS, courses };
+}
+
+export function loadCourses() {
+  return function(dispatch) {
+    return courseApi
+      .loadCourses()
+      .then(courses => dispatch(loadCourseSuccess(courses)))
+      .catch(err => {
+        throw err;
+      });
+  };
 }
